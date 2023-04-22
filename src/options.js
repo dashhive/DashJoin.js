59028911025dd415cbac7c3ad06ff0be7a5fce33
dev <dev@bnull.net>
2023-04-21 20:23:57 -0600

    feat: CCoinJoinClientOptions class

--- src/options.js
+++ src/options.js
@@ -1,105 +1,61 @@
 /**
- * A port of DASH core's CoinJoin client
+ * A port of DASH core's CoinJoin CCoinJoinClientOptions class
  */
+
+
+const DEFAULT_COINJOIN_SESSIONS = 4;
+const DEFAULT_COINJOIN_ROUNDS = 4;
+const DEFAULT_COINJOIN_AMOUNT = 1000;
+const DEFAULT_COINJOIN_DENOMS_GOAL = 50;
+const DEFAULT_COINJOIN_DENOMS_HARDCAP = 300;
+const DEFAULT_COINJOIN_AUTOSTART = false;
+const DEFAULT_COINJOIN_MULTISESSION = false;
 
 
 let Lib = {};
 module.exports = Lib;
-// std::map<const std::string, std::shared_ptr<CCoinJoinClientManager>> coinJoinClientManagers;
-Lib.NetMsgType = require('net-msg.js').NetMsgType;
+/* Application wide mixing options */
+Lib.CCoinJoinClientOptions = {};
+Lib.CCoinJoinClientOptions._instance = null;
+Lib.CCoinJoinClientOptions.GetSessions = function () { return Lib._instance.nCoinJoinSessions; }
+Lib.CCoinJoinClientOptions.GetRounds = function () { return Lib._instance.nCoinJoinRounds; }
+Lib.CCoinJoinClientOptions.GetRandomRounds = function () { return Lib._instance.nCoinJoinRandomRounds; }
+Lib.CCoinJoinClientOptions.GetAmount = function () { return Lib._instance.nCoinJoinAmount; }
+Lib.CCoinJoinClientOptions.GetDenomsGoal = function () { return Lib._instance.nCoinJoinDenomsGoal; }
+Lib.CCoinJoinClientOptions.GetDenomsHardCap = function () { return Lib._instance.nCoinJoinDenomsHardCap; }
+Lib.CCoinJoinClientOptions.IsEnabled = function () { return Lib._instance.fEnableCoinJoin; }
+Lib.IsMultiSessionEnabled = function () { return Lib._instance.fCoinJoinMultiSession; }
 
-//Orig: std::deque<CCoinJoinClientSession> deqSessions GUARDED_BY(cs_deqsessions);
-Lib.deqSessions = {};
-Lib.coinJoinQueue = {};
-Lib.coinJoinQueue.write = function(msg) {
-
+Lib.CCoinJoinClientOptions.SetEnabled = function(fEnabled){
+	Lib._instance.fEnableCoinJoin = fEnabled;
 };
-Lib.coinJoinQueue.dsq = function() {
-	return 'dsq'; // FIXME: return actual dsq message
+Lib.CCoinJoinClientOptions.SetMultiSessionEnabled = function (fEnabled){
+	Lib._instance.fCoinJoinMultiSession = fEnabled;
 };
+Lib.CCoinJoinClientOptions.SetRounds = function (nRounds){
+	Lib._instance.nCoinJoinRounds = nRounds;
+};
+Lib.CCoinJoinClientOptions.SetAmount = function (amount){
+	Lib._instance.nCoinJoinAmount = amount;
+};
+Lib.CCoinJoinClientOptions.Init = function() {
+	Lib._instance = {};
+	Lib._instance.fCoinJoinMultiSession = DEFAULT_COINJOIN_MULTISESSION;
+	Lib._instance.nCoinJoinSessions = DEFAULT_COINJOIN_SESSIONS;
+	Lib._instance.nCoinJoinRounds = DEFAULT_COINJOIN_ROUNDS;
+	Lib._instance.nCoinJoinAmount = DEFAULT_COINJOIN_AMOUNT;
+	Lib._instance.nCoinJoinDenomsGoal = DEFAULT_COINJOIN_DENOMS_GOAL;
+	Lib._instance.nCoinJoinDenomsHardCap = DEFAULT_COINJOIN_DENOMS_HARDCAP;
+}
 
-Lib.vRecv = {};
-
-Lib.CCoinJoinClientManager = {};
-/** 
- * Used to keep track of current status of mixing pool
- */
-//class CCoinJoinClientManager
-    // Keep track of the used Masternodes
-    //std::vector<COutPoint> vecMasternodesUsed;
-
-Lib.vecMasternodesUsed = [];
-//orig: const std::unique_ptr<CMasternodeSync>& m_mn_sync;
-Lib.m_mn_sync = {};
-
-//orig: std::deque<CCoinJoinClientSession> deqSessions GUARDED_BY(cs_deqsessions);
-Lib.deqSessions = {};
-//    std::atomic<bool> fMixing{false};
-Lib.isMixing = false;
-
-/** orig:
-    int nCachedLastSuccessBlock{0};
-    int nMinBlocksToWait{1}; // how many blocks to wait for after one successful mixing tx in non-multisession mode
-    bilingual_str strAutoDenomResult;
-		*/
-
-Lib.nCachedLastSuccessBlock = 0;
-// how many blocks to wait for after one successful mixing tx in non-multisession mode
-Lib.nMinBlocksToWait = 1;
-Lib.strAutoDenomResult = '';
-//orig: CWallet& mixingWallet;
-Lib.walletImpl = require('wallet.js');
-Lib.mixingWallet = Lib.walletImpl.loadFromJSON(require('./data/wallet.json'));
-
-    // Keep track of current block height
-    int nCachedBlockHeight{0};
-
-    bool WaitForAnotherBlock() const;
-
-    // Make sure we have enough keys since last backup
-    bool CheckAutomaticBackup();
-
-public:
-    int nCachedNumBlocks{std::numeric_limits<int>::max()};    // used for the overview screen
-    bool fCreateAutoBackups{true}; // builtin support for automatic backups
-
-    CCoinJoinClientManager() = delete;
-    CCoinJoinClientManager(CCoinJoinClientManager const&) = delete;
-    CCoinJoinClientManager& operator=(CCoinJoinClientManager const&) = delete;
-
-    explicit CCoinJoinClientManager(CWallet& wallet, const std::unique_ptr<CMasternodeSync>& mn_sync) :
-        m_mn_sync(mn_sync), mixingWallet(wallet) {}
-
-    void ProcessMessage(CNode& peer, CConnman& connman, const CTxMemPool& mempool, std::string_view msg_type, CDataStream& vRecv) LOCKS_EXCLUDED(cs_deqsessions);
-
-    bool StartMixing();
-    void StopMixing();
-    bool IsMixing() const;
-    void ResetPool() LOCKS_EXCLUDED(cs_deqsessions);
-
-    bilingual_str GetStatuses() LOCKS_EXCLUDED(cs_deqsessions);
-    std::string GetSessionDenoms() LOCKS_EXCLUDED(cs_deqsessions);
-
-    bool GetMixingMasternodesInfo(std::vector<CDeterministicMNCPtr>& vecDmnsRet) const LOCKS_EXCLUDED(cs_deqsessions);
-
-    /// Passively run mixing in the background according to the configuration in settings
-    bool DoAutomaticDenominating(CTxMemPool& mempool, CConnman& connman, bool fDryRun = false) LOCKS_EXCLUDED(cs_deqsessions);
-
-    bool TrySubmitDenominate(const CService& mnAddr, CConnman& connman) LOCKS_EXCLUDED(cs_deqsessions);
-    bool MarkAlreadyJoinedQueueAsTried(CCoinJoinQueue& dsq) const LOCKS_EXCLUDED(cs_deqsessions);
-
-    void CheckTimeout() LOCKS_EXCLUDED(cs_deqsessions);
-
-    void ProcessPendingDsaRequest(CConnman& connman) LOCKS_EXCLUDED(cs_deqsessions);
-
-    void AddUsedMasternode(const COutPoint& outpointMn);
-    CDeterministicMNCPtr GetRandomNotUsedMasternode();
-
-    void UpdatedSuccessBlock();
-
-    void UpdatedBlockTip(const CBlockIndex* pindex);
-
-    void DoMaintenance(CTxMemPool& mempool, CConnman& connman);
-
-    void GetJsonInfo(UniValue& obj) const LOCKS_EXCLUDED(cs_deqsessions);
+Lib.CCoinJoinClientOptions.GetJsonInfo = function(){
+	return JSON.stringify({
+    "enabled": Lib._instance.fEnableCoinJoin,
+    "multisession": Lib._instance.fCoinJoinMultiSession,
+    "max_sessions": Lib._instance.nCoinJoinSessions,
+    "max_rounds": Lib._instance.nCoinJoinRounds,
+    "max_amount": Lib._instance.nCoinJoinAmount,
+    "denoms_goal": Lib._instance.nCoinJoinDenomsGoal,
+    "denoms_hardcap": Lib._instance.nCoinJoinDenomsHardCap,
+	});
 };
