f04983c76e5e164ca248791f200a7a55a3ea6c2f
dev <dev@bnull.net>
2023-04-27 11:02:26 -0600

    wip: CDataStream

--- src/cdata-stream.js
+++ src/cdata-stream.js
@@ -11,84 +11,68 @@ const CoinType = require("./cointype-constants.js");
 const Vector = require("./vector.js");
 module.exports = Lib;
 
-function CDataStream(args = {
-
-}){
-	let self = this;
+function CDataStream(args = {}) {
+  let self = this;
   this.vch = new Uint8Array();
   this.nReadPos = 0;
-    this.nType = 0;
-    this.nVersion = 0;
+  this.nType = 0;
+  this.nVersion = 0;
 
-    this.Init = function(nTypeIn, nVersionIn) {
-        self.nReadPos = 0;
-        self.nType = nTypeIn;
-        self.nVersion = nVersionIn;
-    };
+  this.Init = function (nTypeIn, nVersionIn) {
+    self.nReadPos = 0;
+    self.nType = nTypeIn;
+    self.nVersion = nVersionIn;
+  };
 
-    this.read = function(pch, nSize) {
-        if (nSize == 0) { return; }
+  this.read = function (pch, nSize) {
+    if (nSize == 0) {
+      return;
+    }
 
-        // Read from the beginning of the buffer
-        let nReadPosNext = self.nReadPos + nSize;
-        if (nReadPosNext > self.vch.size()) {
-            throw new Error("CDataStream::read(): end of data");
-        }
-      //orig: memcpy(pch, &vch[nReadPos], nSize);
-			for(let i=0; i < nSize;i++){
-				self.vch[nReadPos + i] = pch[i];
-			}
-        if (nReadPosNext == self.vch.size())
-        {
-            self.nReadPos = 0;
-            self.vch = new Uint8Array();
-            return;
-        }
-        self.nReadPos = nReadPosNext;
-    };
+    // Read from the beginning of the buffer
+    let nReadPosNext = self.nReadPos + nSize;
+    if (nReadPosNext > self.vch.size()) {
+      throw new Error("CDataStream::read(): end of data");
+    }
+    //orig: memcpy(pch, &vch[nReadPos], nSize);
+    for (let i = 0; i < nSize; i++) {
+      self.vch[nReadPos + i] = pch[i];
+    }
+    if (nReadPosNext == self.vch.size()) {
+      self.nReadPos = 0;
+      self.vch = new Uint8Array();
+      return;
+    }
+    self.nReadPos = nReadPosNext;
+  };
 
-    //void ignore(int nSize)
-    //{
-    //    // Ignore from the beginning of the buffer
-    //    if (nSize < 0) {
-    //        throw std::ios_base::failure("CDataStream::ignore(): nSize negative");
-    //    }
-    //    unsigned int nReadPosNext = nReadPos + nSize;
-    //    if (nReadPosNext >= vch.size())
-    //    {
-    //        if (nReadPosNext > vch.size())
-    //            throw std::ios_base::failure("CDataStream::ignore(): end of data");
-    //        nReadPos = 0;
-    //        vch.clear();
-    //        return;
-    //    }
-    //    nReadPos = nReadPosNext;
-    //}
+  this.write = function (pch, nSize) {
+    // Write to the end of the buffer
+    for (let i = 0; i < nSize; i++) {
+      self.vch[self.vch.length + i] = pch[i];
+    }
+  };
 
-    this.write = function(pch, nSize) {
-        // Write to the end of the buffer
-        self.vch.insert(vch.end(), pch, pch + nSize);
-    };
+  /**
+   * XOR the contents of this stream with a certain key.
+   *
+   * @param[in] key    The key used to XOR the data in this stream.
+   */
+  this.Xor = function (key) {
+    if (key.size() == 0) {
+      return;
+    }
 
-    /**
-     * XOR the contents of this stream with a certain key.
-     *
-     * @param[in] key    The key used to XOR the data in this stream.
-     */
-    this.Xor = function(key) {
-        if (key.size() == 0) {
-            return;
-        }
+    for (let i = 0, j = 0; i != self.size(); i++) {
+      self.vch[i] ^= key[j++];
 
-        for (let i = 0, j = 0; i != self.size(); i++) {
-            self.vch[i] ^= key[j++];
-
-            // This potentially acts on very many bytes of data, so it's
-            // important that we calculate `j`, i.e. the `key` index in this
-            // way instead of doing a %, which would effectively be a division
-            // for each byte Xor'd -- much slower than need be.
-            if (j == key.size())
-                j = 0;
-        }
-    };
-};
+      // This potentially acts on very many bytes of data, so it's
+      // important that we calculate `j`, i.e. the `key` index in this
+      // way instead of doing a %, which would effectively be a division
+      // for each byte Xor'd -- much slower than need be.
+      if (j == key.size()) {
+        j = 0;
+      }
+    }
+  };
+}
