6f942b2c2c9e9b2a0c24ff99dfd804c5cbca76ed
AJ ONeal <aj@therootcompany.com>
2024-04-03 23:54:27 -0600

    chore: change sh indent to 4 spaces

--- utils/dc
+++ utils/dc
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dash-cli -conf="${HOME}"/.dashmate/local_seed/core/dash.conf \
-  $*
+    $*
--- utils/dm-restart.sh
+++ utils/dm-restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for service in local_1 local_2 local_3 local_seed; do
-  dashmate restart --config="$service"
+    dashmate restart --config="$service"
 done
