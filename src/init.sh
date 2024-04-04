5fd7f8f0854dbd4e0981598ec232fc6d2987eba1
AJ ONeal <aj@therootcompany.com>
2024-04-03 23:54:27 -0600

    chore: change sh indent to 4 spaces

--- src/init.sh
+++ src/init.sh
@@ -5,22 +5,22 @@ echo 'base' > ~/.dashjoinjs/current
 DB=~/bin/db
 
 if [[ $1 == "--force" ]]; then
-  dashmate group stop
-  dashmate group reset
-  LOOP=yes
-  while [[ $LOOP == "yes" ]]; do
-    dashmate group start
-    if [[ $? -eq 0 ]]; then
-      LOOP=no
-    fi
-  done
+    dashmate group stop
+    dashmate group reset
+    LOOP=yes
+    while [[ $LOOP == "yes" ]]; do
+        dashmate group start
+        if [[ $? -eq 0 ]]; then
+            LOOP=no
+        fi
+    done
 
-  rm -rf ~/.dashjoinjs/base
+    rm -rf ~/.dashjoinjs/base
 
-  echo 'base' > ~/.dashjoinjs/current
+    echo 'base' > ~/.dashjoinjs/current
 
-  $DB --create-wallets
-  $DB --make-junk-user
+    $DB --create-wallets
+    $DB --make-junk-user
 
 fi
 
@@ -32,19 +32,19 @@ LOOP=yes
 GEN=no
 CTR=0
 while [[ $LOOP == "yes" ]]; do
-  CTR=$((CTR + 1))
-  if [[ $CTR -eq 10 ]]; then
-    LOOP="no"
-    break
-  fi
-  for ID in $(seq 1 3); do
-    $DB --split-utxos=user$ID
-    if [[ $? -ne 0 ]]; then
-      GEN=yes
+    CTR=$((CTR + 1))
+    if [[ $CTR -eq 10 ]]; then
+        LOOP="no"
+        break
     fi
-  done
-  if [[ $GEN == "yes" ]]; then
-    $DB --dash-for-all
-    GEN=no
-  fi
+    for ID in $(seq 1 3); do
+        $DB --split-utxos=user$ID
+        if [[ $? -ne 0 ]]; then
+            GEN=yes
+        fi
+    done
+    if [[ $GEN == "yes" ]]; then
+        $DB --dash-for-all
+        GEN=no
+    fi
 done
--- utils/make-ctags.sh
+++ utils/make-ctags.sh
@@ -7,11 +7,11 @@ rm $TAGS 2> /dev/null
 cd $WEBROOT
 ls *.cpp *.h *.hpp */*p */*.h | grep -v ':' 2> /dev/null > $WEBROOT/cscope.files
 ctags -R -f $TAGS --links=no --totals=yes \
-  --exclude='*.py' --exclude=Makefile* \
-  --exclude='*.js' \
-  --totals=no \
-  --tag-relative=yes \
-  --if0=no \
-  --fields=+a+f+i+K+n+s+S+z+t \
-  --C++-kinds=+f+c+e-g-l+m-u+v &
+    --exclude='*.py' --exclude=Makefile* \
+    --exclude='*.js' \
+    --totals=no \
+    --tag-relative=yes \
+    --if0=no \
+    --fields=+a+f+i+K+n+s+S+z+t \
+    --C++-kinds=+f+c+e-g-l+m-u+v &
 cscope -R -b -i $WEBROOT/cscope.files &
