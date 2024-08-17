19b856b8474e319364d74d40b5135e795a93d67d
AJ ONeal <aj@therootcompany.com>
2024-08-17 06:47:39 -0600

    fix+test: expose raw transaction_unsigned

--- tests/dsf.js
+++ tests/dsf.js
@@ -1,10 +1,11 @@
 'use strict';
 
-let Assert = require('node:assert/strict');
+// let Assert = require('node:assert/strict');
 let Fs = require('node:fs/promises');
 let Path = require('node:path');
 
-let Parser = require('../parser.js');
+let DashP2P = require('../public/dashp2p.js');
+let DashJoin = require('../public/dashjoin.js');
 // TODO copy .utils.bytesToHex rather than depend on it
 let DashKeys = require('dashkeys');
 
@@ -12,29 +13,29 @@ async function test() {
 	let fixtureDsfBytes = await readFixtureHex('dsf');
 	// let fixtureDsqJson = require('../fixtures/dsf.json');
 
-	let header = Parser.parseHeader(fixtureDsfBytes);
+	let header = DashP2P.parsers.header(fixtureDsfBytes);
 	if (header.command !== 'dsf') {
 		throw new Error(
 			`sanity fail: should have loaded 'dsf' fixture, but got '${header.command}'`,
 		);
 	}
 
-	let payload = fixtureDsfBytes.subarray(Parser.HEADER_SIZE);
+	let payload = fixtureDsfBytes.subarray(DashP2P.sizes.HEADER);
 	// TODO verify
 	//   - a chosen subset of our offered inputs (e.g. we offer 9, but 3 are selected)
 	//   - that equally many of our offered outputs are selected (e.g. 3 = 3)
 	//   - that the satoshi values of our outputs match coin for coin
-	let dsf = Parser.parseDsf(payload);
+	let dsf = DashJoin.parsers.dsf(payload);
 	console.log(new Date(), '[debug] dsf obj:', dsf);
 	if ('string' !== typeof dsf.transaction_unsigned) {
-		throw new Error("'.transactionUnsigned' should exist as a hex string");
+		throw new Error("'.transaction_unsigned' should exist as a hex string");
 	}
 	let txLen = dsf.transaction_unsigned.length / 2;
-	let expectedLen = header.payloadSize - Parser.SESSION_ID_SIZE;
+	let expectedLen = header.payloadSize - DashJoin.sizes.SESSION_ID;
 	console.log(new Date(), '[debug] dsf len:', txLen);
 	if (txLen !== expectedLen) {
 		throw new Error(
-			`expected '.transactionUnsigned' to represent ${header.payloadSize} bytes, but got ${txLen}`,
+			`expected '.transaction_unsigned' to represent ${header.payloadSize} bytes, but got ${txLen}`,
 		);
 	}
 
